d7919e385baa3c72a862c2557fa0ae3dc0166566
Amerrick4-RRC <amerrick4@academic.rrc.ca>
2026-02-02 17:18:38 -0600

    Made some repairs and commented on area's that I have not cover

--- assignment1_code_sample_1.py
+++ assignment1_code_sample_1.py
@@ -1,26 +1,27 @@
 import os
 import pymysql
 from urllib.request import urlopen
-
-db_config = {
-    'host': 'mydatabase.com',
-    'user': 'admin',
-    'password': 'secret123'
-}
+import requests
 
 def get_user_input():
     user_input = input('Enter your name: ')
-    return user_input
+    if len(user_input) <= 1:
+        return f"User name [{user_input}] must be more than one character"
+    else:
+        return user_input
 
 def send_email(to, subject, body):
+    #repair to us smtp and encryption, os.system is vulnerable to injection with f string use, validate inputs
     os.system(f'echo {body} | mail -s "{subject}" {to}')
 
 def get_data():
-    url = 'http://insecure-api.com/get-data'
-    data = urlopen(url).read().decode()
-    return data
+    url = 'https://insecure-api.com/get-data' 
+    responce = requests.get(url, timeout=3)
+    responce.raise_for_status()
+    return responce.text
 
 def save_to_db(data):
+    # Add appropriate data integrity checks prior to insertion
     query = f"INSERT INTO mytable (column1, column2) VALUES ('{data}', 'Another Value')"
     connection = pymysql.connect(**db_config)
     cursor = connection.cursor()
